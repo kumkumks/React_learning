7395ee2f062e3be49de9503daeefc8bbde58ca9c
Kumkum Sharma <kumkumks671@gmail.com>
2026-02-20 16:09:32 +0530

    feat: update Card component to accept props for title and value; enhance Dashboard to render multiple Card instances

--- day1/src/component/card/card.jsx
+++ day1/src/component/card/card.jsx
@@ -1,19 +1,18 @@
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { Card_data } from './card'
-import Badge from './bagde'
+import Bagde from './bagde'
 
-const Card = () => {
+const Card = (Props) => {
   return (
-    <div className="card w-60 border border-green-700 rounded-xl overflow-hidden shadow-sm"> 
+    <div className="card w-60 border h-72 border-green-700 rounded-xl overflow-hidden shadow-sm"> 
       <div className="bg-green-700 p-4" style={{background: 'linear-gradient(to right, rgb(21, 128, 61), rgb(22, 101, 52))'}}>
-        <h5 className="text-white text-sm font-medium">Total Projects</h5>
-        <h2 className="text-white text-5xl font-medium mt-2">24</h2>
+        <h5 className="text-white text-sm font-medium">{Props.title}</h5>
+        <h2 className="text-white text-5xl font-medium mt-2">{Props.value}</h2>
         <div className='flex items-center gap-2 mt-4'>
           {/* badge */}
           <div className='flex items-center gap-1 border-yellow-100 border-2 py-1 px-2 rounded-full'>
-            <Badge />
+            <Bagde />
           </div>
           <p className='text-yellow-100 font-extralight text-sm'>Increased from last month</p>
         </div>
--- day1/src/component/dashboard.jsx
+++ day1/src/component/dashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Card from './card'
+import { Card_data } from './card/card'
 
 const Dashboard = () => {
   return (
-    <div className='bg-gray-100 p-2.5 h-dvh mt-2 w-full'>
-        <Card />
+    <div className='bg-gray-100 p-2.5 flex flex-wrap gap-4 mt-2 w-full'>
+      {
+        Card_data.map(card => (
+          <Card key={card.id} title={card.title} value={card.value} />
+        ))
+      }
     </div>
   )
 }
