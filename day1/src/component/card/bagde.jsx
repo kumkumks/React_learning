487e3d7ebb4a680bef1fc243d6d0543bd690bbf4
Kumkum Sharma <kumkumks671@gmail.com>
2026-02-21 11:51:16 +0530

    feat: refactor Bagde and Card components; enhance styling and layout for better visual consistency

--- day1/src/component/card/bagde.jsx
+++ day1/src/component/card/bagde.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
-
 const Bagde = ({trend}) => {
   return (
-    <>
-         <span className='text-yellow-100 font-medium text-sm'>{trend}%</span>
-         <FontAwesomeIcon icon={faAngleUp} className='text-yellow-100 text-xs' />
-    </>
+    <div className='badge-wrapper'>
+      <FontAwesomeIcon icon={faAngleUp} className='badge-icon' />
+      <span className='badge-text'>{trend}</span>
+    </div>
   )
 }
 
--- day1/src/component/card/card.jsx
+++ day1/src/component/card/card.jsx
@@ -6,16 +6,16 @@ import { Card_data } from './card'
 
 const Card = (props) => {
   return (
-    <div className="card w-60 border border-green-700 rounded-xl overflow-hidden shadow-sm">
-      <div className="bg-green-700 p-4" style={{ background: 'linear-gradient(to right, rgb(21, 128, 61), rgb(22, 101, 52))' }}>
-        <h5 className="text-white text-sm font-medium">{props.title}</h5>
-        <h2 className="text-white text-5xl font-medium mt-2">{props.value}</h2>
+    <div className="card">
+      <div className="card-header">
+        <h5 className="text-black text-sm font-medium">{props.title}</h5>
+        <h2 className=" text-black text-5xl font-medium mt-2">{props.value}</h2>
         <div className='flex items-center gap-2 mt-4'>
           {/* badge */}
-          <div className='flex items-center gap-1 border-yellow-100 border-2 py-1 px-2 rounded-full w-max'>
-                <Bagde key={props.id} trend={props.trend} />
+          <div className='badge-container'>
+            <Bagde key={props.id} trend={props.trend} />
           </div>
-          <p className='text-yellow-100 font-extralight text-sm'>Increased from last month</p>
+          <p className='badge-description'>Increased from last month</p>
         </div>
       </div>
     </div>
