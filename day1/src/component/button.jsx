6e7f70acf2a11765ebdeadb30123277466cc39e7
Kumkum Sharma <kumkumks671@gmail.com>
2026-02-25 16:20:44 +0530

    feat: add chart component for project analytics; update Dashboard to include BarChart and enhance Card styling

--- day1/src/component/button.jsx
+++ day1/src/component/button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({text}) => {
   return (
     <div className='flex items-center w-auto mt-1'>
        <button className='button-style me-2'>Add Project</button>
--- day1/src/component/dashboard.jsx
+++ day1/src/component/dashboard.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Card from './card/card.jsx'
 import { Card_data } from './card/card.js'
+import BarChart from './chart.jsx'
 
 const Dashboard = () => {
   return (
-    <div className='bg-gray-100 p-2.5 flex flex-wrap gap-4 mt-2 w-full'>
-      {
-        Card_data.map(card => (
-          <Card key={card.id} title={card.title} value={card.value} trend={card.trendValue} />
-        ))
-      }
+    <div className='bg-gray-100 mt-2 w-full flex justify-center lg:justify-start flex-wrap  p-2.5  gap-4'>
+        {
+          Card_data.map(card => (
+            <Card key={card.id} title={card.title} value={card.value} trend={card.trendValue} />
+          ))
+        }
+      <div className="bg-white flex w-full h-96 rounded-xl shadow p-4">
+        <BarChart />
+        
+      </div> 
     </div>
   )
 }
