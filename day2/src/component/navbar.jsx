9f39870c118d8d757901e1a41324003ae67cc41a
Kumkum Sharma <kumkumks671@gmail.com>
2026-03-11 11:51:38 +0530

    style: update Navbar component styles for improved layout and visibility

--- day2/src/component/navbar.jsx
+++ day2/src/component/navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-red-100'>
-        <h1>Navbar</h1>
+    <div className='bg-red-100 text-white h-16 flex items-center justify-center font-bold text-2xl shadow-lg'>
+        <h1>My Website</h1>
     </div>
   )
 }
